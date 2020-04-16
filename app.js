7f0de9e4e522d5937c5314560642de7ee4c67162
harshithva <harshith11032001@gmail.com>
2020-04-16 22:18:14 +0530

    Cafes can be added and delete

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const cafeList = document.querySelector('#cafe-list');
-
+const form = document.querySelector('#add-cafe-form');
 //create element and render cafe
 function renderCafe(doc) {
     let li = document.createElement("li");
     let name = document.createElement("span");
     let city = document.createElement("span");
+    let cross = document.createElement("div");
 
     li.setAttribute('data-id', doc.id)
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent = 'x';
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
+
     cafeList.appendChild(li);
 
+    //delete data
+    cross.addEventListener('click', (event) => {
+        event.stopPropagation();
+        let id = event.target.parentElement.getAttribute("data-id");
+        db.collection("cafes").doc(id).delete();
+
+    })
 }
 
+//Getting data
 db.collection("cafes").get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
@@ -23,3 +35,12 @@ db.collection("cafes").get().then((snapshot) => {
     });
 })
 
+//saving data
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    db.collection("cafes").add({
+        name: form.name.value,
+        city: form.city.value
+    })
+})
